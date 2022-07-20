83f9c44bbb06a531bf17c3da64b02a81bd538d36
Monito <phil.menard1668@gmail.com>
2022-07-20 14:22:49 -0300

    Add the function to return the computer choice. Rework all the functions to get choice to make it more easy to understand.

--- javascript.js
+++ javascript.js
@@ -6,14 +6,13 @@ let keepGoing = true;
 // while(keepGoing)    {
 
 // on demande au joueur quel choix il veut faire
-const choicePlayer = setPlayerChoice(); 
+const playerChoice = setPlayerChoice(); 
 
-//     - le joueur choisit une réponse entre les 3 options proposées, faire attention aux erreurs et réponses incorrectes
-//     - la réponse est stockée dans une variable const
-const answerPlayer = getPlayerChoice(choicePlayer);
+// le joueur choisit une réponse entre les 3 options proposées et la réponse est stockée dans une variable const
+const answerPlayer = getPlayerChoice(playerChoice);
 
 //     - l'ordinateur choisit une réponse, qui sera aléatoire donc, entre les 3 options proposées
-//             FAIRE UNE FONCTION QUI RETURN LE CHOIX DE L'ORDINATEUR (afficher une string pour confirmer le choix de l'ordinateur (ex : "l'ordinateur a choisi 'pierre'"))
+const answerComputer = getComputerChoice();
 //     - le choix est stocké dans une variable
 
 //     - Le programme compare les résultats avec des conditions (perdant / gagnant / égalité) et affiche le message correspondant
@@ -33,21 +32,48 @@ const answerPlayer = getPlayerChoice(choicePlayer);
 //FAIRE UNE FONCTION QUI RETURN LE CHOIX DU JOUEUR (afficher une string pour confirmer le choix du joueur (ex : "vous avez choisi 'pierre'"))
 
 function getPlayerChoice(choice)    {
-    console.log(`You picked ${choice}`);
+    alert(`You picked ${choice}`);
     return choice;
 }
 
 function setPlayerChoice()    {
+    // on initialise une boucle qui va vérifier la valeur entrée par le joueur
     let keepGoing = true;
     while(keepGoing){
-        let choicePlayer = prompt("Make your choice by typing the associated number! Rocks(1), Papers(2) or Scissors(3)?");
-        if (choicePlayer == "1" || choicePlayer == "2" || choicePlayer == "3")  {
+
+        let value = prompt("Make your choice by typing the associated number! Rocks(1), Papers(2) or Scissors(3)?");
+        let choice = setChoice(value);
+        if (value == "1" || value == "2" || value == "3")   {
             keepGoing = false;
-            return choicePlayer;
+            return choice;
         }
-        else    {
-            alert("Please pick a number between 1 and 3");
-        }
-        console.log(choicePlayer);
     }
+}
+
+function setChoice(choice)    {
+    
+    if (choice == "1")  {   // quand le joueur a choisi, on change la valeur de keepGoing pour terminer la boucle et on return le choix correspondant
+        choice = "Rocks";
+        return choice;
+    }
+    else if(choice == "2")    {
+        choice = "Papers";
+        return choice;
+    }
+    else if(choice == "3")    {
+        choice = "Scissors";
+        return choice;
+    }
+    else    {   // si la valeur est différente de 1, 2 ou 3 alors on affiche un message d'erreur et on recommence la boucle
+        alert("Please pick a number between 1 and 3");
+        return choice;
+    }
+    
+}
+
+function getComputerChoice()    {   // return le choix de l'ordinateur
+    let random = Math.floor(Math.random() * 3) + 1;
+    let value = setChoice(random);
+    alert(`The computer picked ${value}`);
+    return value;
 }
